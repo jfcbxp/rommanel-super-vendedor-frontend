da6f5b0aa1ba31330290ceb9af21338202e91eee
Fhilype <fhilype@hotmail.com>
2023-05-18 15:47:05 -0300

    Push_1

---
+++ src/components/modals/dialog/index.tsx
@@ -0,0 +1,27 @@
+import { GestureResponderEvent, Modal, View, Text } from "react-native";
+import { DialogStyles as styles } from "./styles";
+import { Button } from "../../buttons/button";
+
+interface Properties {
+    visible: boolean | undefined;
+    dismiss: ((event: GestureResponderEvent) => void) | undefined;
+    title: string;
+    content: string;
+}
+
+export function Dialog(properties: Properties) {
+    return (
+        <Modal {...properties}
+            visible={properties.visible}
+            transparent={true}
+            animationType="fade">
+            <View style={styles.container}>
+                <View style={styles.field}>
+                    <Text style={styles.title}>{properties.title}</Text>
+                    <Text style={styles.content}>{properties.content}</Text>
+                    <Button onPress={properties.dismiss} title="CONTINUAR" />
+                </View>
+            </View>
+        </Modal>
+    );
+}
--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,23 +1,30 @@
 import axios from "axios";
 
 export function http(token?: string) {
-  const _baseURL = "http://192.168.0.239:9191/super-vendedor";
-  if (token) {
-    return axios.create({
-      baseURL: _baseURL,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      timeout: 20000,
-    });
-  } else {
-    return axios.create({
-      baseURL: _baseURL,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      timeout: 20000,
-    });
-  }
+    // test : "26.214.78.128"
+    // production : "192.168.0.239"
+    const host = "192.168.0.239"
+    const _baseURL = `http://${host}:9191/super-vendedor`
+    if (token) {
+        return axios.create({
+            baseURL: _baseURL,
+            headers: {
+                "Content-Type": "application/json",
+                "ngrok-skip-browser-warning": "24022023",
+                "Bypass-Tunnel-Reminder": "27022023",
+                Authorization: `Bearer ${token}`,
+            },
+            timeout: 20000,
+        });
+    } else {
+        return axios.create({
+            baseURL: _baseURL,
+            headers: {
+                "Content-Type": "application/json",
+                "ngrok-skip-browser-warning": "24022023",
+                "Bypass-Tunnel-Reminder": "27022023",
+            },
+            timeout: 20000,
+        });
+    }
 }
