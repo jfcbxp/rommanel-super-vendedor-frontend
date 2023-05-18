9160a44bb3218dfa0f1742ad5993860c8e6166cc
Joao Braga <jfcbxp@gmail.com>
2023-05-18 08:50:07 -0300

    remove headers

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -7,8 +7,6 @@ export function http(token?: string) {
       baseURL: _baseURL,
       headers: {
         "Content-Type": "application/json",
-        "ngrok-skip-browser-warning": "24022023",
-        "Bypass-Tunnel-Reminder": "27022023",
         Authorization: `Bearer ${token}`,
       },
       timeout: 20000,
@@ -18,8 +16,6 @@ export function http(token?: string) {
       baseURL: _baseURL,
       headers: {
         "Content-Type": "application/json",
-        "ngrok-skip-browser-warning": "24022023",
-        "Bypass-Tunnel-Reminder": "27022023",
       },
       timeout: 20000,
     });
