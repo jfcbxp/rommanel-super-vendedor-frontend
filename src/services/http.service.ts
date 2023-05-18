cd96b1befa6a00b2a3c19b4581df163cc6841222
Joao Braga <jfcbxp@gmail.com>
2023-05-17 22:23:49 -0300

    update backend url

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,27 +1,27 @@
 import axios from "axios";
 
 export function http(token?: string) {
-    const _baseURL = "http://26.214.78.128:9191/super-vendedor"
-    if (token) {
-        return axios.create({
-            baseURL: _baseURL,
-            headers: {
-                "Content-Type": "application/json",
-                "ngrok-skip-browser-warning": "24022023",
-                "Bypass-Tunnel-Reminder": "27022023",
-                Authorization: `Bearer ${token}`,
-            },
-            timeout: 20000,
-        });
-    } else {
-        return axios.create({
-            baseURL: _baseURL,
-            headers: {
-                "Content-Type": "application/json",
-                "ngrok-skip-browser-warning": "24022023",
-                "Bypass-Tunnel-Reminder": "27022023",
-            },
-            timeout: 20000,
-        });
-    }
+  const _baseURL = "http://192.168.0.239:9191/super-vendedor";
+  if (token) {
+    return axios.create({
+      baseURL: _baseURL,
+      headers: {
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "24022023",
+        "Bypass-Tunnel-Reminder": "27022023",
+        Authorization: `Bearer ${token}`,
+      },
+      timeout: 20000,
+    });
+  } else {
+    return axios.create({
+      baseURL: _baseURL,
+      headers: {
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "24022023",
+        "Bypass-Tunnel-Reminder": "27022023",
+      },
+      timeout: 20000,
+    });
+  }
 }
