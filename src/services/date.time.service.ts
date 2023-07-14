0afa78d41f43b21a26b8507a1378f2d92598e7d7
Joao Braga <jfcbxp@gmail.com>
2023-07-14 19:20:26 -0300

    upd date.time.service

--- src/services/date.time.service.ts
+++ src/services/date.time.service.ts
@@ -1,88 +1,69 @@
-export const getDateNow = () => {
-    const date = new Date();
-    let day = date.getDate().toString();
-    if (day.toString().length == 1) {
-        day = '0' + day;
-    }
-    let month = (date.getMonth() + 1).toString();
-    if (month.toString().length == 1) {
-        month = '0' + month;
-    }
-    let year = date.getFullYear().toString();
-    return `${day}/${month}/${year}`;
+const MAX_DAY = 31;
+const MAX_MONTH = 12;
+const MAX_HOUR = 23;
+const MAX_MINUTE = 59;
+
+const isNumeric = (value: any): boolean => {
+    return !isNaN(parseFloat(value)) && isFinite(value);
 };
 
-export const fixTime = (time: string) => {
-    time = time.trim().replace(/\s/g, '').replace(':', '');
-    if (time.length == 1) {
-        time = `0${time}:00`;
-    } else if (time.length == 2) {
-        time = `${time}:00`;
-    } else {
-        let hour = time.slice(0, 2);
-        let minute = time.slice(2, 4);
-        time = `${hour}:${minute}`;
+const formatValue = (value: number): string => {
+    return String(value).padStart(2, '0');
+};
+
+const extractDateValues = (date: string): [number, number, number] => {
+    const [day, month, year] = date.split('/').map(Number);
+    return [day, month, year];
+};
+
+export const getDateNow = (): string => {
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear();
+    return `${formatValue(day)}/${formatValue(month)}/${year}`;
+};
+
+export const fixTime = (time: string): string => {
+    const trimmedTime = time.replace(/\s|:/g, '');
+
+    const hour = trimmedTime.slice(0, 2).padStart(2, '0');
+    const minute = trimmedTime.slice(2, 4).padStart(2, '0');
+
+    return `${hour}:${minute}`;
+};
+
+export const adjustDate = (date?: string): string => {
+    if (!date || date.length !== 10) {
+        return date!;
     }
-    return time;
+
+    const [day, month, year] = extractDateValues(date);
+
+    if (!isNumeric(day) || !isNumeric(month) || !isNumeric(year)) {
+        return date;
+    }
+
+    const currentYear = new Date().getFullYear();
+    const adjustedDay = formatValue(Math.min(day, MAX_DAY));
+    const adjustedMonth = formatValue(Math.min(month, MAX_MONTH));
+
+    if (adjustedDay === '00' || adjustedMonth === '00') {
+        return '';
+    }
+
+    return `${adjustedDay}/${adjustedMonth}/${currentYear}`;
 };
 
-export const adjustDate = (date?: string) => {
-    if (!date) {
-        return ''
-    } else {
-        const dateArray: string[] = date.split('/');
-        let day = parseInt(dateArray[0], 10);
-        let month = parseInt(dateArray[1], 10);
-        let year = parseInt(dateArray[2], 10);
+export const adjustTime = (time?: string): string => {
+    if (!time || time.length !== 5) {
+        return time!;
+    }
 
-        if (date.length !== 10) {
-            return date
-        };
+    const [hour, minute] = time.split(':').map(Number);
 
-        const currentDate = new Date();
+    const adjustedHour = formatValue(Math.min(hour, MAX_HOUR));
+    const adjustedMinute = formatValue(Math.min(minute, MAX_MINUTE));
 
-        if (day > 31) {
-            day = 31
-        };
-
-        if (month > 12) {
-            month = 12
-        };
-
-        if (year > currentDate.getFullYear()) {
-            year = currentDate.getFullYear()
-        };
-
-        const _day = day.toString().padStart(2, '0');
-        const _month = month.toString().padStart(2, '0');
-
-        return `${_day}/${_month}/${year}`;
-    };
-
-};
-
-export const adjustTime = (time?: string) => {
-    if (!time) {
-        return ''
-    } else {
-        const timeArray: string[] = time.split(':');
-        let hour = parseInt(timeArray[0], 10);
-        let minute = parseInt(timeArray[1], 10);
-
-        if (time.length !== 5) return time;
-
-        if (hour > 23) {
-            hour = 23
-        };
-
-        if (minute > 59) {
-            minute = 59
-        };
-
-        const _hour = hour.toString().padStart(2, '0');
-        const _minute = minute.toString().padStart(2, '0');
-
-        return `${_hour}:${_minute}`;
-    };
-
+    return `${adjustedHour}:${adjustedMinute}`;
 };
