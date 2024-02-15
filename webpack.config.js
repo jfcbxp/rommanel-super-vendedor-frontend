d67c133ec46f8701e2111cb3bb944b7893abe5c2
jfcbxp <jfcbxp@gmail.com>
2024-02-15 12:08:33 -0300

    upd expo webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
-const createExpoWebpackConfigAsync = require("@expo/webpack-config");
-const path = require("path");
-const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = async function (env, argv) {
-  // Set by expo-cli during `expo build:web`
-  const isEnvProduction = env.mode === "production";
+    // Set by expo-cli during `expo build:web`
+    const isEnvProduction = env.mode === 'production';
 
-  // Create the default config
-  const config = await createExpoWebpackConfigAsync(env, argv);
+    // Create the default config
+    const config = await createExpoWebpackConfigAsync(env, argv);
 
-  if (isEnvProduction) {
-    config.plugins.push(
-      // Generate a service worker script that will precache, and keep up to date,
-      // the HTML & assets that are part of the webpack build.
-      new WorkboxWebpackPlugin.InjectManifest({
-        swSrc: path.resolve(__dirname, "src/service-worker.js"),
-        dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
-        exclude: [
-          /\.map$/,
-          /asset-manifest\.json$/,
-          /LICENSE/,
-          /\.js\.gz$/,
-          // Exclude all apple touch and chrome images because they're cached locally after the PWA is added.
-          /(apple-touch-startup-image|chrome-icon|apple-touch-icon).*\.png$/,
-        ],
-        // Bump up the default maximum size (2mb) that's precached,
-        // to make lazy-loading failure scenarios less likely.
-        // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      })
-    );
-  }
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify'),
+        url: require.resolve('url'),
+    });
+    config.resolve.fallback = fallback;
 
-  return config;
+    if (isEnvProduction) {
+        config.plugins.push(
+            // Generate a service worker script that will precache, and keep up to date,
+            // the HTML & assets that are part of the webpack build.
+            new WorkboxWebpackPlugin.InjectManifest({
+                swSrc: path.resolve(__dirname, 'src/service-worker.js'),
+                dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+                exclude: [
+                    /\.map$/,
+                    /asset-manifest\.json$/,
+                    /LICENSE/,
+                    /\.js\.gz$/,
+                    // Exclude all apple touch and chrome images because they're cached locally after the PWA is added.
+                    /(apple-touch-startup-image|chrome-icon|apple-touch-icon).*\.png$/,
+                ],
+                // Bump up the default maximum size (2mb) that's precached,
+                // to make lazy-loading failure scenarios less likely.
+                // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+            }),
+        );
+    }
+
+    return config;
 };
