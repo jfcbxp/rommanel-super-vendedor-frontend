70ddad91d196608533472fd772348029c3a0781a
jfcbxp <jfcbxp@gmail.com>
2024-03-01 09:03:33 -0300

    ♻️ refactor: wallet status 30 days

--- src/enums/wallet.status.enum.ts
+++ src/enums/wallet.status.enum.ts
@@ -2,7 +2,7 @@ export enum WalletStatusEnum {
     ANIVERSARIANTES_DIA = 'Aniversariantes do dia',
     ATIVO = 'Ativo',
     BLOQUEADO_MEDIA = 'Bloqueado media de vendas',
-    INATIVO_30_DIAS = '30 dias sem compra',
+    INATIVO_30_DIAS = '30 dias sem compras',
     INATIVO_60_DIAS = '60 dias sem compras',
     INATIVO_90_DIAS = '90 dias sem compras',
     INATIVO = 'Inativo',
